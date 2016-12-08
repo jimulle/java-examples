8a243a737a99dfaa50681d6a197752e14efd6d49
jim ulle <julle@gaig.com>
2016-12-08 10:06:20 -0500

    hello world

---
+++ hello_world/HelloWorld.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello, " + args[0]);
+        // single line comment
+        /*
+           multi
+           line
+           comment
+        */
+        /**
+         * this is a javadoc comment
+         */
+    }
+}
