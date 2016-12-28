f8582b1e40bf7d8a052e8e509aac94a7b6ee4ff5
jim ulle <julle@gaig.com>
2016-12-28 15:08:48 -0500

    IO WordCount :: additional analysis added

--- io/WordCount.java
+++ io/WordCount.java
@@ -22,13 +22,21 @@ public class WordCount
     private static void analyze(Boolean aFile) throws java.io.IOException {
         int wordCount = 0;
         int characterCount = 0;
+        int nonwspCharacterCount = 0;
+        int lowLetterCount = 0;
+        int upLetterCount = 0;
+        int numCount = 0;
+        int specCharCount = 0;
         Boolean end = false;
         while (!end) {
             int i = System.in.read();
             if (i == -1) {
                 end = true;
                 continue;
-            } else if (i == '\r') {
+            } else {
+                nonwspCharacterCount++;
+            }
+            if (i == '\r') {
                 continue;
             } else if (i == '\n') {
                 if (characterCount > 0) // this is currently counting double returns in a file as a word
@@ -40,8 +48,25 @@ public class WordCount
                 wordCount++;
             } else {
                 characterCount++;
+                if (i >= 'a' && i <= 'z') {
+                    lowLetterCount++;
+                } else if (i >= 'A' && i <= 'Z') {
+                    upLetterCount++;
+                } else if (i >= '0' && i <= '9') {
+                    numCount++;
+                } else {
+                    specCharCount++;
+                }
             }
         }
-        System.out.println("The text contains " + characterCount + " characters and " + wordCount + " words.");
+        System.out.println("Text Analysis: ");
+        System.out.println("   total characters:             " + nonwspCharacterCount);
+        System.out.println("      not counting white spaces: " + characterCount);
+        System.out.println("      just letters:              " + (lowLetterCount + upLetterCount));
+        System.out.println("         upper case letters:     " + upLetterCount);
+        System.out.println("         lower case letters:     " + lowLetterCount);
+        System.out.println("      numbers:                   " + numCount);
+        System.out.println("      special characters:        " + specCharCount);
+        System.out.println("   total words:                  " + wordCount);
     }
 }
