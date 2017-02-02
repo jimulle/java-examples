c2aa3d41ccfd234037e11a5305bd92e8136230d2
jim ulle <julle@gaig.com>
2017-02-02 09:51:05 -0500

    playing with webservices (grizzly, maven & jersey)    url: http://www.oracle.com/webfolder/technetwork/tutorials/obe/java/griz_jersey_intro/Grizzly-Jersey-Intro.html

---
+++ grizzly-ws/jersey-service/src/main/java/com/example/rest/JimUlle.java
@@ -0,0 +1,25 @@
+package com.example.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "jimulle" path)
+ */
+@Path("jimulle")
+public class JimUlle {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "jim ulle was here...";
+    }
+}
